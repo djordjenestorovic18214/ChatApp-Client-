b3517f3858e5ad6bcc47cafe5b81e09e9dcae70e
djordjenestorovic18214 <nestordjole@gmail.com>
2016-12-24 22:22:20 +0100

    udp implemented, chat is now hybrid app

--- ChatAppClient/src/Client.java
+++ ChatAppClient/src/Client.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -12,6 +15,12 @@ public class Client implements Runnable {
 	static BufferedReader inStreamFromServer = null;
 	static BufferedReader console = null;
 	static boolean kraj = false;
+	//udp
+	static DatagramPacket packetFromServer=null;
+	static DatagramSocket socketUDP = null;
+	static byte[] dataForServer = new byte[1024];
+	static byte[] dataFromServer = new byte[1024]; 
+	static String host;
 
 	public static void main(String[] args) {
 		try {
@@ -25,6 +34,10 @@ public class Client implements Runnable {
 			console = new BufferedReader(new InputStreamReader(System.in));
 			outStreamToServer = new PrintStream(communicationSocket.getOutputStream());
 			inStreamFromServer = new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
+			
+			//udp
+			socketUDP=new DatagramSocket();
+			outStreamToServer.println(socketUDP.getLocalPort()+" "+InetAddress.getLocalHost());
 
 			new Thread(new Client()).start();
 
@@ -32,23 +45,32 @@ public class Client implements Runnable {
 				outStreamToServer.println(console.readLine());
 			}
 			communicationSocket.close();
-			
-		}catch (UnknownHostException e) {
+
+		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host");
 		} catch (IOException e) {
 			System.err.println(e);
 		}
 	}
-	
+
 	@Override
 	public void run() {
-		String textlineFromServer;
-
+		String textlineFromServer, udp;
+		packetFromServer= new  DatagramPacket(dataFromServer, dataFromServer.length);
+		
 		try {
 			while ((textlineFromServer = inStreamFromServer.readLine()) != null) {
+				//udp part
+				if(textlineFromServer.startsWith("udp")){
+					packetFromServer= new  DatagramPacket(dataFromServer, dataFromServer.length);
+					socketUDP.receive(packetFromServer);
+					udp=new String(packetFromServer.getData()).trim();
+					System.out.println(udp);
+					continue;
+				}
 
 				System.out.println(textlineFromServer);
-				
+
 				if (textlineFromServer.startsWith("•••Goodbye ")) {
 					kraj = true;
 					return;
